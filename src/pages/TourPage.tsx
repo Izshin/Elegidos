95efb0d4499358236aea168a2a217973ee5c189b
Izshin <ivanfernandezlimarquez757@gmail.com>
2025-12-03 03:02:32 +0100

    Si en desplegao va

--- src/pages/TourPage.tsx
+++ src/pages/TourPage.tsx
@@ -24,7 +24,7 @@ const TourPage = () => {
         style={{ display: 'block', maxWidth: '100%', padding: '2rem' }}
       >
         <motion.img
-          src="public/images/Tour placeholder.jpg"
+          src="/images/Tour%20placeholder.jpg"
           alt="Tour Dates Placeholder"
           whileHover={{
             boxShadow: '0 0 30px var(--color-accent)',
